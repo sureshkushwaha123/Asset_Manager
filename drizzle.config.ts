704aa9d7f214bcb8d688b6e3a3d50bab23971a36
sureshkushwaha123 <sp.kushawaha12@gmail.com>
2026-04-05 22:44:26 +0530

    first commit

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,7 @@
+import 'dotenv/config';
 import { defineConfig } from "drizzle-kit";
+
+console.log("ENV VALUE:", process.env.DATABASE_URL); // 👈 ADD THIS
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL, ensure the database is provisioned");
